4e013129eb00d9970c79477032373a5bb038dacb
steeldevlol <steeldev87@gmail.com>
2025-08-10 15:03:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v1.1.1"
 export SCRIPT_RELEASE="v1.1.1"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/steeldevlol/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
